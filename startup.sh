68c6c3d8b9c09484748f43f82857554e8f7fba8a
Jarrett Horton <jghorton14@gmail.com>
2020-10-11 16:56:30 -0400

    testing

--- startup.sh
+++ startup.sh
@@ -10,10 +10,25 @@ echo "--------------" >> $LOGPATH
 echo $DATE >> $LOGPATH
 echo $UUID >> $LOGPATH
 
-### Move previous recording and rename file to USB ###
-mv /home/pi/Documents/videos/video.h264 $LEXARMOUNTPATH/
-cd $LEXARMOUNTPATH
-mv video.h264 $UUID.h264
+wget -q --spider http://google.com
+if [ $? -eq 0 ]; then
+  ### Online ###
+  echo "Attempting to save to s3" >> $LOGPATH
+
+  ### Transfer all videos to S3 ###
+
+  ### Delete Videos from USB ###
+else
+  ### Offline ###
+  echo "Saving to USB" >> $LOGPATH
+
+  ### Move previous recording and rename file to USB ###
+  mv /home/pi/Documents/videos/video.h264 $LEXARMOUNTPATH/
+  cd $LEXARMOUNTPATH
+  mv video.h264 $UUID.h264
+fi
+
+echo "Finished"
 
 # ### Start recording.. ###
-python /home/pi/Documents/videos/record.py
+# python /home/pi/Documents/videos/record.py
